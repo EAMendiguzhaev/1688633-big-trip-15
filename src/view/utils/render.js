7dedd8a5da0abdbe597ea96cea0c0e2ce685f82a
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-08-12 19:32:15 +0300

    Минорное изменение

--- src/view/utils/render.js
+++ src/view/utils/render.js
@@ -15,12 +15,14 @@ const render = (container, child, place) => {
   }
 
   switch (place) {
-    case RenderPosition.AFTERBEGIN:
+    case RenderPosition.AFTERBEGIN: {
       container.prepend(child);
       break;
-    case RenderPosition.BEFOREEND:
+    }
+    case RenderPosition.BEFOREEND: {
       container.append(child);
       break;
+    }
   }
 };
 
