62712e2c7493595430baf08bc26b116c4cb466ff
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-09-05 12:58:47 +0300

    Реализовал обратную связь от сервера

--- src/api.js
+++ src/api.js
@@ -3,6 +3,8 @@ import PointsModel from './model/points.js';
 const Method = {
   GET: 'GET',
   PUT: 'PUT',
+  POST: 'POST',
+  DELETE: 'DELETE',
 };
 
 class Api {
@@ -38,6 +40,24 @@ class Api {
     })
       .then(Api.toJSON)
       .then(PointsModel.adaptToClient);
+  }
+
+  addPoint(point) {
+    return this._load({
+      url: 'points',
+      method: Method.POST,
+      body: JSON.stringify(PointsModel.adaptToServer(point)),
+      headers: new Headers({ 'Content-Type': 'application/json' }),
+    })
+      .then(Api.toJSON)
+      .then(PointsModel.adaptToClient);
+  }
+
+  deletePoint(point) {
+    return this._load({
+      url: `points/${point.id}`,
+      method: Method.DELETE,
+    });
   }
 
   _load({ url, method = Method.GET, body = null, headers = new Headers() }) {
